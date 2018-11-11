d222e455450d7dea2eb9f04449523c8dfc723c8c
Patrick <patrick@responsive.co.za>
2018-11-11 20:23:53 +0200

    Fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,79 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import '../../static/fonts/OstrichSans-Medium.eot';
+import '../../static/fonts/OstrichSans-Medium.svg';
+import '../../static/fonts/OstrichSans-Medium.ttf';
+import '../../static/fonts/OstrichSans-Medium.woff';
+import '../../static/fonts/OstrichSans-Medium.woff2';
+import '../../static/fonts/Georgia.eot';
+import '../../static/fonts/Georgia.svg';
+import '../../static/fonts/Georgia.ttf';
+import '../../static/fonts/Georgia.woff';
+import '../../static/fonts/Georgia.woff2';
+import '../../static/fonts/RedVevet.eot';
+import '../../static/fonts/RedVevet.svg';
+import '../../static/fonts/RedVevet.ttf';
+import '../../static/fonts/RedVevet.woff';
+import '../../static/fonts/RedVevet.woff2';
+import './font-face.css';
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <Layout>
-        <section className="section">
+        <section className="section pages">
           <div className="container">
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
+            {posts.map(({ node: post }) => (
+              <div
+                className="content"
+                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                key={post.id}
+              >
+                <p>
+                  <Link className="has-text-primary" to={post.fields.slug}>
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-small" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </div>
+            ))}
           </div>
         </section>
       </Layout>
-    )
+    );
   }
 }
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+      edges: PropTypes.array
+    })
+  })
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -76,4 +91,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
