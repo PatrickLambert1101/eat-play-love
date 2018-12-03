2a4a37fc95ff4bcb6fb134dfe27d43e8481800e0
Patrick <patrick@responsive.co.za>
2018-12-03 07:45:50 +0200

    asdffasdf

--- src/cms/preview-templates/OfferingsPagePreview.js
+++ src/cms/preview-templates/OfferingsPagePreview.js
@@ -1,22 +1,19 @@
-export default OfferingsPagePreview;
-
-
-import React from 'react'
-import PropTypes from 'prop-types'
-import { OfferingsPageTemplate } from '../../templates/offerings-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { OfferingsPageTemplate } from '../../templates/offerings-page';
 
 const OfferingsPagePreview = ({ entry, widgetFor }) => (
   <OfferingsPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
-)
+);
 
 OfferingsPagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  widgetFor: PropTypes.func,
-}
+  widgetFor: PropTypes.func
+};
 
-export default OfferingsPagePreview
+export default OfferingsPagePreview;
