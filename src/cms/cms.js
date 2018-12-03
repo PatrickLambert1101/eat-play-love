2fbae6574f2bc1319d8b322f434cc123f08b79be
Patrick <patrick@responsive.co.za>
2018-12-03 07:41:43 +0200

    asdf

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
 import CMS from 'netlify-cms';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
-import OfferingsPagePreviedew from './preview-templates/OfferingsPagePreview';
+import OfferingsPagePreview from './preview-templates/OfferingsPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
