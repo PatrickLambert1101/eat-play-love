6858ba52b912870bfbfc268892718d8991486773
Patrick <patrick@responsive.co.za>
2018-12-03 07:36:45 +0200

    Clean config.yml

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,7 @@ import CMS from 'netlify-cms';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import OfferingsPagePreviedew from './preview-templates/OfferingsPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import OfferingsPagePreview from './preview-templates/OfferingsPagePreview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('offerings', OfferingsPagePreviedew);
 CMS.registerPreviewTemplate('offerings', OfferingsPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
