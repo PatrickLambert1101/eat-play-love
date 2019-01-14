e03836dded5d543ec694ec148d94275725e5210d
Patrick <patrick@responsive.co.za>
2019-01-14 21:21:12 +0200

    fix

--- src/templates/offerings-post.js
+++ src/templates/offerings-post.js
@@ -12,6 +12,9 @@ const GalleryImages = styled.div`
   margin-top: 40px;
   flex-wrap: wrap;
   justify-content: space-between;
+  @media (max-width: 480px) {
+    flex-direction: column;
+  }
   & > div {
     flex: 1 0 30%;
     margin: 10px;
