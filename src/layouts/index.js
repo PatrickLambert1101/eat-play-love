9d69f336713c4c37f8ce72a450d1294f389cfd36
Patrick <patrick@responsive.co.za>
2019-01-17 11:35:33 +0200

    Moved font location

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,24 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Transition from './Transition';
 import './all.sass';
+import '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+import '../../static/fonts/OstrichSans-Medium.eot';
+import '../../static/fonts/OstrichSans-Medium.svg';
+import '../../static/fonts/OstrichSans-Medium.ttf';
+import '../../static/fonts/OstrichSans-Medium.woff';
+import '../../static/fonts/OstrichSans-Medium.woff2';
+import '../../static/fonts/Georgia.eot';
+import '../../static/fonts/Georgia.svg';
+import '../../static/fonts/Georgia.ttf';
+import '../../static/fonts/Georgia.woff';
+import '../../static/fonts/Georgia.woff2';
+import '../../static/fonts/RedVevet.eot';
+import '../../static/fonts/RedVevet.svg';
+import '../../static/fonts/RedVevet.ttf';
+import '../../static/fonts/RedVevet.woff';
+import '../../static/fonts/RedVevet.woff2';
+import './font-face.css';
 
 const TemplateWrapper = ({ children, location }) => (
   <StaticQuery
--- src/pages/retreats.js
+++ src/pages/retreats.js
@@ -5,22 +5,6 @@ import styled from 'styled-components';
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
 import '../../node_modules/slick-carousel/slick/slick.css';
 import '../../node_modules/slick-carousel/slick/slick-theme.css';
-import '../../static/fonts/OstrichSans-Medium.eot';
-import '../../static/fonts/OstrichSans-Medium.svg';
-import '../../static/fonts/OstrichSans-Medium.ttf';
-import '../../static/fonts/OstrichSans-Medium.woff';
-import '../../static/fonts/OstrichSans-Medium.woff2';
-import '../../static/fonts/Georgia.eot';
-import '../../static/fonts/Georgia.svg';
-import '../../static/fonts/Georgia.ttf';
-import '../../static/fonts/Georgia.woff';
-import '../../static/fonts/Georgia.woff2';
-import '../../static/fonts/RedVevet.eot';
-import '../../static/fonts/RedVevet.svg';
-import '../../static/fonts/RedVevet.ttf';
-import '../../static/fonts/RedVevet.woff';
-import '../../static/fonts/RedVevet.woff2';
-import './font-face.css';
 
 export default class RetreatsPage extends React.Component {
   render() {
