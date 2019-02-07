378985f8cd1978c48eed578f3673be3bc0ddc104
Patrick <patrick@responsive.co.za>
2019-02-07 17:44:21 +0200

    2nd go

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,37 +16,21 @@ export default class Navbar extends React.Component {
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     this.toggleMenu = this.toggleMenu.bind(this);
 
-    this.verticalAnimation = this.verticalAnimation.bind(this);
-
     this.layoutContents = React.createRef();
     this.transitionCover = React.createRef();
   }
 
-  verticalAnimation = ({ length }, direction) => {
-    const directionTo = direction === 'up' ? '-100%' : '100%';
-    const directionFrom = direction === 'up' ? '100%' : '-100%';
-
-    // convert ms to s for gsap
-    const seconds = length;
-
-    return new TimelineMax()
-      .set(this.transitionCover, {
-        y: directionFrom
-      })
-      .to(this.transitionCover, seconds / 2, {
-        y: '0%',
-        ease: Power1.easeInOut
-      })
-      .set(this.layoutContents, { opacity: 0 })
-      .to(this.transitionCover, seconds / 2, {
-        y: directionTo,
-        ease: Power1.easeIn
-      });
-  };
-
-  test(entry, node) {
+  in(entry, node) {
     return new TimelineMax().staggerFrom(
-      node.querySelectorAll('h2, p, a, pre'),
+      node.querySelectorAll('h1, p, a, pre'),
+      1,
+      { opacity: 0, y: '+=50' },
+      0.1
+    );
+  }
+  out(entry, node) {
+    return new TimelineMax().staggerFrom(
+      node.querySelectorAll('h1, p, a, pre'),
       1,
       { opacity: 0, y: '+=50' },
       0.1
@@ -91,13 +75,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/"
             className="navbar-item"
@@ -121,13 +104,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/"
           >
@@ -136,13 +118,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/events"
           >
@@ -151,13 +132,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/retreats"
           >
@@ -166,13 +146,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/about"
           >
@@ -181,13 +160,12 @@ export default class Navbar extends React.Component {
           <TransitionLink
             to="/page-2"
             exit={{
-              length: 1,
-              trigger: ({ exit }) => this.verticalAnimation(exit, 'down'),
-              state: { test: 'exit state' }
+              delay: 0.5,
+              trigger: ({ entry, node }) => this.in(entry, node)
             }}
             entry={{
               delay: 0.5,
-              trigger: ({ entry, node }) => this.test(entry, node)
+              trigger: ({ entry, node }) => this.out(entry, node)
             }}
             to="/contact"
           >
