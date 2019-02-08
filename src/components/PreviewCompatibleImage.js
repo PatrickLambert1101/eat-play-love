9c3986f5d33a071c08e1ee83a574f8bc35256e79
Patrick <patrick@responsive.co.za>
2019-02-08 09:43:23 +0200

    remove proptypes

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -4,21 +4,18 @@ import Img from 'gatsby-image';
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
   const { alt = '', childImageSharp } = imageInfo;
-  console.log(
-    'TCL: PreviewCompatibleImage -> childImageSharp',
-    childImageSharp
-  );
+
   if (!!childImageSharp) {
     return <Img fluid={childImageSharp.fluid} alt={alt} />;
   }
   return null;
 };
 
-PreviewCompatibleImage.propTypes = {
-  imageInfo: PropTypes.shape({
-    alt: PropTypes.string,
-    childImageSharp: PropTypes.object
-  }).isRequired
-};
+// PreviewCompatibleImage.propTypes = {
+//   imageInfo: PropTypes.shape({
+//     alt: PropTypes.string,
+//     childImageSharp: PropTypes.object
+//   }).isRequired
+// };
 
 export default PreviewCompatibleImage;
