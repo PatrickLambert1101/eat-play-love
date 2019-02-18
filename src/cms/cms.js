6c2870a249af39b0a56eeacb03acfeb79684cd5e
Patrick <patrick@responsive.co.za>
2019-02-18 12:34:24 +0200

    sta

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,6 @@ import CMS from 'netlify-cms';
 import EventsPostPreview from './preview-templates/EventsPostPreview';
 // import BlogPostPreview from './preview-templates/BlogPostPreview';
 
-CMS.registerPreviewTemplate('events post', EventsPostPreview);
+CMS.registerPreviewTemplate('events-post', EventsPostPreview);
 // CMS.registerPreviewTemplate('offerings-page', OfferingsPagePreview);
 // CMS.registerPreviewTemplate('blog', BlogPostPreview);
