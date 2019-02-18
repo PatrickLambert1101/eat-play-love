bce4fa3f25604740af55eb543c2c3399c4996bf3
Patrick <patrick@responsive.co.za>
2019-02-18 12:22:07 +0200

    asdf

--- src/cms/preview-templates/EventsPostPreview.js
+++ src/cms/preview-templates/EventsPostPreview.js
@@ -7,6 +7,9 @@ const EventsPostPreview = ({ entry, widgetFor }) => (
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
+    leadText={entry.getIn(['data', 'leadText'])}
+    review={entry.getIn(['data', 'review'])}
+    author={entry.getIn(['data', 'author'])}
   />
 );
 
