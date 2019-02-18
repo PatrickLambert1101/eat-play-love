d93fe3eb135318eb70c8161ce53a0df3166dce24
Patrick <patrick@responsive.co.za>
2019-02-18 12:38:55 +0200

    asdf

--- src/cms/preview-templates/EventsPostPreview.js
+++ src/cms/preview-templates/EventsPostPreview.js
@@ -3,9 +3,6 @@ import { EventsPostTemplate } from '../../templates/events-post';
 
 const EventsPostPreview = ({ entry, widgetFor }) => (
   <EventsPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
     leadText={entry.getIn(['data', 'leadText'])}
     review={entry.getIn(['data', 'review'])}
